15eb65472a86bf94a7001018abebac001ddef8c7
ceezyyy <ceezyyy11@gmail.com>
2020-08-03 12:41:25 +0800

    update Java / optional

--- Java/Optional/code/optional-demo/src/com/ceezyyy/Main.java
+++ Java/Optional/code/optional-demo/src/com/ceezyyy/Main.java
@@ -1,8 +1,42 @@
 package com.ceezyyy;
 
+import java.util.Optional;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        /*
+         * To create an empty Optional object, we simply need to use its empty() static method
+         *
+         * */
+        Optional<Object> empty = Optional.empty();
+        System.out.println(empty.isPresent());  // false
+
+
+        /*
+         * However, the argument passed to the of() method can't be null.
+         * Otherwise, we'll get a NullPointerException:
+         *
+         * */
+        Optional<String> hello = Optional.of("hello");
+        System.out.println(hello.isPresent());  // true
+
+
+        /*
+         * But, in case we expect some null values, we can use the ofNullable() method
+         *
+         * */
+        Optional<Object> opt = Optional.ofNullable(null);
+        System.out.println(opt.isPresent());  // false
+        opt = Optional.ofNullable("world");
+        System.out.println(opt.isPresent());  // true
+
+
+
+        
+
+
+
     }
 }

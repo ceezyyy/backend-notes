ea17b1d235b1a8d9aa019e899ba150543b9bba99
ceezyyy <ceezyyy11@gmail.com>
2020-08-13 17:43:59 +0800

    update design pattern / intro

---
+++ Core/DesignPatterns/code/design-pattern/interface-segregation-principle/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        Teacher teacher = new Teacher();
+
+        student.exam();
+        teacher.teach();
+    }
+
+}
